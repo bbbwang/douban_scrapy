a23bdd4493ba95c2ec0f0fa32bc266d9ca856389
Alimjan Mettursun <amettursun@ebay.com>
2019-09-19 20:17:40 +0800

    设置user_agent代理

--- douban/settings.py
+++ douban/settings.py
@@ -52,9 +52,10 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'douban.middlewares.DoubanDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   # 'douban.middlewares.DoubanDownloaderMiddleware': 543,
+    'douban.middlewares.my_useragent': 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
